5adf953c7f8b0bc36f3481110ae861ba964155b0
KielLagmay <37663637+KielLagmay@users.noreply.github.com>
2021-03-02 11:29:48 +0800

    Update sqsdequeue.py

--- sqsdequeue.py
+++ sqsdequeue.py
@@ -8,15 +8,16 @@ import time
 
 from flatten_json import flatten
 from hashlib import sha512
+from bs4 import BeautifulSoup
+import unicodedata
 
 # Create SQS client
 sqs = boto3.client('sqs' )
 
+def get_sha(s):
+    return sha512(str.encode(s)).hexdigest()
 
 def anonymize_data(json_data_flat, cols):
-    def get_sha(s):
-        return sha512(str.encode(s)).hexdigest()
-
     for col in cols:
         try:
             json_data_flat[col] = get_sha(str(json_data_flat[col]))
@@ -25,6 +26,16 @@ def anonymize_data(json_data_flat, cols):
 
     return json_data_flat
 
+def anonymize_html_data(json_data_flat, cols):
+    for col in cols:
+        try:
+            html_data = unicodedata.normalize("NFKD", BeautifulSoup(json_data_flat[col], 'lxml').get_text(strip = True, separator = " "))
+            
+            json_data_flat[col] = html_data
+        except:
+            continue
+    
+    return json_data_flat
 
 def get_event_specific_sensitive_columns(event_name):
     sensitive_columns = {
@@ -52,12 +63,45 @@ def get_event_specific_sensitive_columns(event_name):
         return sensitive_columns[event_name]
     except(KeyError):
         return []
+    
+def get_html_data_columns(event_name):
+    html_data_columns = {
+        'account_notification_created': ['body_message', 'body_subject'],
+        'assignment_created': ['body_description', 'body_title'], 
+        'assignment_updated': ['body_description', 'body_title'],
+        'attachment_created': ['body_display_name', 'body_filename'],
+        'attachment_deleted': ['body_display_name', 'body_filename'],  
+        'attachment_updated': ['body_display_name', 'body_filename', 'body_old_display_name'],
+        'discussion_entry_created': ['body_text'], 
+        'discussion_entry_submitted': ['body_text'],
+        'discussion_topic_created': ['body_body', 'body_title'], 
+        'discussion_topic_updated': ['body_body', 'body_title'],
+        'learning_outcome_created': ['body_description', 'body_display_name', 'body_short_description', 'body_title'], 
+        'learning_outcome_group_created': ['body_description', 'body_title'], 
+        'learning_outcome_group_updated': ['body_description', 'body_title'], 
+        'learning_outcome_result_created': ['body_title'],
+        'learning_outcome_result_updated': ['body_title'],
+        'learning_outcome_updated': ['body_description', 'body_display_name', 'body_short_description', 'body_title'], 
+        'plagiarism_resubmit': ['body_body'], 
+        'submission_comment_created': ['body_body'], 
+        'submission_created': ['body_body'], 
+        'submission_updated': ['body_body'], 
+        'syllabus_updated': ['body_old_syllabus_body', 'body_syllabus_body'],
+        'wiki_page_created': ['body_body', 'body_title'],
+        'wiki_page_deleted': ['body_title'],
+        'wiki_page_updated': ['body_body', 'body_old_body', 'body_old_title', 'body_title']
+    }
+
+    try:
+        return html_data_columns[event_name]
+    except(KeyError):
+        return []
 
 
 def anonymize_canvas_data(json_data):
     json_data_flat = flatten(json_data)
     json_data_anon = anonymize_data(json_data_flat, ['metadata_user_login', 'metadata_hostname', 'metadata_client_ip', 'metadata_url', 'metadata_referrer', 'metadata_context_sis_source_id'] + get_event_specific_sensitive_columns(json_data_flat['metadata_event_name']))
-    return json_data_anon
+    return anonymize_html_data(json_data_anon, get_html_data_columns(json_data_anon['metadata_event_name']))
 
 
 def dequeue(queue_url):
