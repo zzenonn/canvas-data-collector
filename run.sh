7701b448d2e3c31f0cd573abb5458c941b75ad2a
Miguel Zenon Nicanor L. Saavedra <miguel.saavedra@obf.ateneo.edu>
2021-03-11 11:16:47 +0800

    Optimize runner

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
-#!/bin/sh
+#!/bin/sh -x
 
-test -d /home/ssm-user/canvas/data/$(date +\%F) || mkdir -p /home/ssm-user/canvas/data/$(date +\%F) 
+TODAY=$(date +\%F)
+TIME_NOW=$(date +\%H-\%M-\%S)
+
+test -d /home/ssm-user/canvas/data/$TODAY || mkdir -p /home/ssm-user/canvas/data/$TODAY
 
 /home/ssm-user/canvas/sqsdequeue.py "https://sqs.ap-northeast-2.amazonaws.com/829585657211/canvas-live-events" | \
-ifne tee /home/ssm-user/canvas/data/$(date +\%F)/$(date +\%H-\%M-\%S).jsonl > /dev/null && \
-rclone copy /home/ssm-user/canvas/data/ admu-gdrive: && \
-rm -r /home/ssm-user/canvas/data/*
+ifne tee /home/ssm-user/canvas/data/$TODAY/$TIME_NOW.jsonl > /dev/null && \
+rclone copy /home/ssm-user/canvas/data/$TODAY/$TIME_NOW.jsonl admu-gdrive:$TODAY && \
+rm -r /home/ssm-user/canvas/data/$TODAY/$TIME_NOW.jsonl
